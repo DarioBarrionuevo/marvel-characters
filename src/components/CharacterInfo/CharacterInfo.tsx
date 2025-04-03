344e092dee23e31872d26f0d29682ff6167e59b2
DarioBarrionuevo <dariobafran@gmail.com>
2025-04-03 17:37:47 +0200

    Add comics component

--- src/components/CharacterInfo/CharacterInfo.tsx
+++ src/components/CharacterInfo/CharacterInfo.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FC, useEffect, useState } from "react";
 import Text from "../Text/Text";
 import { Character } from "@/types/character";
---
+++ src/components/Comics/Comics.tsx
@@ -0,0 +1,93 @@
+import { Comic } from "@/types/comic";
+import * as media from "../../theme/media-queries";
+import styled from "styled-components";
+import Text from "../Text/Text";
+import { FC } from "react";
+import typographyStyles from "@/theme/typography";
+
+export interface PropTypes {
+  comics: Comic[];
+}
+const Comics: FC<PropTypes> = ({ comics }) => {
+  return (
+    <StyledSection>
+      <Container>
+        <StyledHeading level="h2">Comics</StyledHeading>
+
+        <List>
+          {comics.map((comic: Comic) => (
+            <p key={comic.id}>{comic.id}</p>
+            // <ComicCard comic={comic} />
+          ))}
+        </List>
+      </Container>
+    </StyledSection>
+  );
+};
+
+export default Comics;
+
+const StyledSection = styled.section`
+  display: flex;
+  justify-content: center;
+  padding: var(--spacing-48) var(--spacing-none) var(--spacing-24);
+`;
+
+const Container = styled.div`
+  width: 100%;
+
+  ::-webkit-scrollbar {
+    width: 2px;
+    height: 4px;
+    background-color: var(--colors-light-gray);
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--colors-marvel-red);
+  }
+
+  ${media.gteMediumMedia} {
+    max-width: 96rem;
+  }
+`;
+
+const StyledHeading = styled(Text)`
+  text-transform: uppercase;
+
+  ${media.lteSmallMedia} {
+    ${typographyStyles.h3};
+  }
+
+  ${media.smallMedia} {
+    padding: var(--spacing-none) var(--spacing-48);
+  }
+  ${media.lteExtraSmallMedia} {
+    padding: var(--spacing-none) var(--spacing-16);
+  }
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: var(--spacing-16);
+  overflow-x: auto;
+  list-style: none;
+  margin: var(--spacing-none);
+  padding: var(--spacing-24) var(--spacing-none);
+  padding-bottom: var(--spacing-12);
+
+  ${media.smallMedia} {
+    & > :first-child {
+      margin-left: var(--spacing-48);
+    }
+    & > :last-child {
+      margin-right: var(--spacing-48);
+    }
+  }
+  ${media.lteExtraSmallMedia} {
+    & > :first-child {
+      margin-left: var(--spacing-16);
+    }
+    & > :last-child {
+      margin-right: var(--spacing-16);
+    }
+  }
+`;
